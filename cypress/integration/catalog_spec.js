697849cdada65baabfab091fad2089981caa8852
Neil <neil@skywire.co.uk>
2020-08-17 13:41:07 +0100

    Create catalog spec

---
+++ cypress/integration/catalog_spec.js
@@ -0,0 +1,27 @@
+describe('Catalog Test', () => {
+    it('Visit a category', () => {
+        cy.visit('/')
+
+        cy.get("#ui-id-4").should('contain', 'Women').click()
+
+        cy.contains('h1', 'Women');
+
+        cy.get('.product-item').should('have.length.at.least', 4);
+    });
+
+    it.only('Add a product to the cart', () => {
+        cy.server();
+        cy.route('POST', '**/checkout/**').as('addToCart')
+
+        cy.visit('/radiant-tee.html')
+
+        cy.get('.page-title').should('contain.text', 'Radiant Tee');
+        cy.get('#option-label-size-143-item-168').click()
+        cy.get('#option-label-color-93-item-56').click()
+        cy.get('#product-addtocart-button').click();
+
+        cy.wait('@addToCart').then((xhr) => {
+            assert.equal(xhr.status, 200);
+        })
+    });
+})
