5164c606af18c4dd0bbf1980683b4cf860a32b4e
Neil <neil@skywire.co.uk>
2020-08-18 08:41:33 +0100

    Implement login and shipping step

--- cypress/integration/checkout_spec.js
+++ cypress/integration/checkout_spec.js
@@ -1,4 +1,14 @@
-describe.only('Checkout - Critical Path - Guest', () => {
+const shippingAddress = {
+    firstname: "Randy",
+    lastname: "Savage",
+    country_id: 'GB',
+    street: "123 Fake St",
+    city: "FakeTown",
+    postcode: 'A1 1AA',
+    telephone: '01234567890',
+}
+
+describe('Checkout - Critical Path - Guest', () => {
     beforeEach(() => {
         Cypress.Cookies.preserveOnce('PHPSESSID');
     })
@@ -17,6 +27,7 @@ describe.only('Checkout - Critical Path - Guest', () => {
 
     it('View Cart', () => {
         cy.visit('/checkout/cart').contains('Proceed to Checkout');
+        cy.screenshot();
     });
 
     it('Guest Email', () => {
@@ -26,7 +37,32 @@ describe.only('Checkout - Critical Path - Guest', () => {
 
         cy.get('input[name="username"]:visible').type('a@a.com');
 
-        // cy.get('[type="submit"]').click();
+        cy.get('.continue[type="submit"]:first').click();
+    });
+
+    it('Shipping Step', () => {
+
+        cy.visit('/checkout#shipping');
+
+        Object.entries(shippingAddress).forEach((keyValuePair) => {
+            let [field, value] = keyValuePair;
+            let selector = `:input[name^="${field}"]:visible`;
+
+            if (field === 'country_id') {
+                cy.get(selector).select(value);
+            } else {
+                cy.get(selector).type(value);
+            }
+
+            cy.get(selector).should('have.value', value);
+        });
+
+        cy.get(':input[value="flatrate_flatrate"]').check().should('be.checked');
+
+        cy.get('#shipping-method-buttons-container :input[type="submit"]').click();
+
+        cy.contains('Payment Method');
+
         cy.screenshot();
     });
 })
