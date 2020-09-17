114344f35ceb4201880d034719309461e63820b3
Neil <neil@skywire.co.uk>
2020-09-17 09:57:51 +0100

    Add step traversal test

--- cypress/integration/guest_checkout_spec.js
+++ cypress/integration/guest_checkout_spec.js
@@ -15,7 +15,7 @@ describe('Critical Path - Checkout', () => {
         Cypress.Cookies.preserveOnce('PHPSESSID');
     });
 
-    before(() => {
+    beforeEach(() => {
         setupCart(['radiantTeeAddToCart']);
     });
 
@@ -36,6 +36,35 @@ describe('Critical Path - Checkout', () => {
 
         cy.contains('Payment Method');
         // cy.get('#checkmo:visible').check();
+        cy.get('#billing-address-same-as-shipping-checkmo').should('be.checked');
+
+        cy.get('button.checkout[type="submit"]:visible').click();
+
+        cy.contains('Thank you for your purchase!');
+        cy.contains('Create an Account');
+    });
+
+    it('Guest Checkout with step traversal', () => {
+        cy.visit('/checkout');
+        cy.contains('Order Summary');
+        cy.contains('Email Address');
+
+        cy.get('input[name="username"]:visible').type('a@a.com');
+        cy.get('.continue[type="submit"]:first').click();
+
+        addressHandler(shippingAddress);
+
+        // goto payment step
+        cy.get(':input[type="submit"]:visible').click();
+        cy.contains('Payment Method');
+
+        // go back to shipping
+        cy.get('.opc-progress-bar-item-shipping').click();
+        cy.contains('Shipping Address');
+
+        // back to payment
+        cy.get(':input[type="submit"]:visible').click();
+        cy.contains('Payment Method');
         cy.get('#billing-address-same-as-shipping-checkmo').should('be.checked');
 
         cy.get('button.checkout[type="submit"]:visible').click();
